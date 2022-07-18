80359e21ed7fcb9fdf7be171c701986057967b3d
Kumar Gaurav <kmrgrv119@gmail.com>
2022-07-18 16:57:04 +0530

    Adding TypeScript support and its config file

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -2,7 +2,16 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: 'bundle.js',
