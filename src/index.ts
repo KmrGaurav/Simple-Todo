a634b04589792255011d2f161bc0e22ad66174c7
Kumar Gaurav <kmrgrv119@gmail.com>
2022-07-22 14:52:42 +0530

    Adding root element in index.html file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 console.log('Hello! World');
+
+const root = document.getElementById('root')!;
+
+const heading = document.createElement('h1');
+heading.innerHTML = 'Simple To Do';
+
+root.appendChild(heading);
