3c703fe00ece0deaa832970771d479c9adeef056
Kumar Gaurav <kmrgrv119@gmail.com>
2022-07-22 16:38:21 +0530

    Adding scss support and removing css support

--- webpack/dev.config.ts
+++ webpack/dev.config.ts
@@ -16,8 +16,8 @@ const config: Configuration | WebpackOptionsNormalized = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.ts$/,
--- webpack/prod.config.ts
+++ webpack/prod.config.ts
@@ -8,8 +8,8 @@ const config: Configuration = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.ts$/,
