103ffa12f20279cca83de626cc109ff1a499bf82
Kumar Gaurav <kmrgrv119@gmail.com>
2022-07-22 10:19:46 +0530

    Installing eslint and adding its config file

--- webpack/dev.config.ts
+++ webpack/dev.config.ts
@@ -1,38 +1,38 @@
-import path from "path";
-import { Configuration, WebpackOptionsNormalized } from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path';
+import { Configuration, WebpackOptionsNormalized } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: Configuration | WebpackOptionsNormalized = {
-    mode: "development",
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
-        },
-        port: 3000,
-        open: true,
-        hot: true,
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    entry: "./src/index.ts",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: "ts-loader",
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "src/index.html",
-            inject: "body",
-        }),
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
     ],
-    output: {
-        path: path.resolve(__dirname, "../dist"),
-        filename: "bundle.js",
-        clean: true,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
 };
 
 export default config;
