f3b9f783cd360ea40a0d22a86f5ce6c3627972fc
Kumar Gaurav <kmrgrv119@gmail.com>
2022-07-22 11:22:15 +0530

    Installing prettier and adding its config file

--- webpack/dev.config.ts
+++ webpack/dev.config.ts
@@ -3,36 +3,36 @@ import { Configuration, WebpackOptionsNormalized } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: Configuration | WebpackOptionsNormalized = {
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
     },
-    port: 3000,
-    open: true,
-    hot: true,
-  },
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body',
-    }),
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
 };
 
 export default config;
--- webpack/prod.config.ts
+++ webpack/prod.config.ts
@@ -3,28 +3,28 @@ import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: Configuration = {
-  mode: 'production',
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
+    mode: 'production',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body',
-    }),
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-  },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+    },
 };
 
 export default config;
